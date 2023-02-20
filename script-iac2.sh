83f11578381b485de90585aca47eb1d9ea3c67ef
CARLOS PABST PRILLWITZ <cprill@gmail.com>
2023-02-20 18:49:30 -0300

    Create script-iac2.sh
    Criado o script Apache 1.0

---
+++ script-iac2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Atualizando..."
+apt-get update
+apt-get upgrade -y
+apt-get install apache2 -y
+apt-get install unzip -y
+
+echo "Importando a Aplicação e copiando..."
+cd /tmp
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+unzip main.zip
+cd linux-site-dio-main
+cp -R * /var/www/html
+
+echo "Finalizado!"
